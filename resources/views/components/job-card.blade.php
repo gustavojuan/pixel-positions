33f24de92e9337cb39ea9be4e693dad30f8fdc1a
gustavojuan <gustavojuan@gmail.com>
2024-07-11 19:58:46 +0200

    feat:layout and components vamos por tags

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,23 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+
+    <div class="self-start text-sm">Laracast</div>
+
+    <div class="py-8 font-bold">
+        <div>Video Producer</div>
+        <div>Full Time - From $60,000</div>
+    </div>
+
+
+    <div class="flex justify-between items-center mt-auto ">
+
+        <div>
+            <x-tag>Remote</x-tag>
+            <x-tag>Remote</x-tag>
+            <x-tag>Remote</x-tag>
+        </div>
+
+        <img src="http://placehold.it/42/42" alt="" class="rounded-xl">
+    </div>
+
+
+</div>
