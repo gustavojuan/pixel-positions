84ca87e71d9874568bc4f36d7bfd16424a417540
Gustavo Juan <gustavojuandev@gmail.com>
2024-07-12 09:35:34 +0200

    feat:tag section

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,1 +1,1 @@
-<a href="" class="bg-white/10 px-2 py-1 rounded-xl text-xs hover:bg-white/25 transition-colores duration-300">Tag</a>
+<a href="" class="bg-white/10 px-2 py-1 rounded-xl text-xs hover:bg-white/25 transition-colores duration-300">{{$slot}}</a>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,18 +14,21 @@
             </div>
 
         </section>
+
         <section>
-
             <x-section-heading>Tags</x-section-heading>
-
-
+            <div class="mt-6 space-x-1">
+                <x-tag>Element 1</x-tag>
+                <x-tag>Element 1</x-tag>
+                <x-tag>Element 1</x-tag>
+                <x-tag>Element 1</x-tag>
+                <x-tag>Element 1</x-tag>
+                <x-tag>Element 1</x-tag>
+            </div>
         </section>
 
         <section>
-
             <x-section-heading>Recent Jobs</x-section-heading>
-
-
         </section>
 
 
