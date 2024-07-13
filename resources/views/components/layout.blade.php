c05696b89a5258f557bcfdc04f9dfca8d1ba2b79
gustavojuan <gustavojuan@gmail.com>
2024-07-13 09:28:29 +0200

    feat: auth and search jobs

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,14 +7,15 @@
     <title>{{ config('APP_NAME') }}</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@300,400,500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@300,400,500&display=swap"
+        rel="stylesheet">
     @vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
 
-<body class="bg-black text-white font-hanken-grotesk">
+<body class="bg-black text-white font-hanken-grotesk pb-20">
 
     <div class="px-10">
-        <nav class="flex justify-between items-center  py-4 border-b border-white/10" >
+        <nav class="flex justify-between items-center  py-4 border-b border-white/10">
             <div>
                 <a href="/">
                     <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="" width="120">
@@ -28,7 +29,27 @@
 
             </div>
             <div>
-                <a href="">Post a job</a>
+
+                @auth
+                    <div>
+                        <a href="/jobs/create">Post a job</a>
+                    </div>
+
+                    <form method="POST" action="/logout">
+                        @csrf
+                        @method('DELETE')
+
+                        <button>Log Out</button>
+                    </form>
+
+                @endauth
+
+                @guest
+                    <div class="space-x-6 font-bold">
+                        <a href="/login"> Login</a>
+                        <a href="/register"> Register</a>
+                    </div>
+                @endguest
             </div>
         </nav>
 
