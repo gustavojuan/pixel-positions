6cb37f1f01b2dfe0e005264eaee459afcce57a7c
Gustavo Juan <gustavojuandev@gmail.com>
2024-07-15 07:56:20 +0200

    feat: show tag route

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Tag extends Model
 {
@@ -12,4 +14,10 @@ class Tag extends Model
     protected $fillable = [
         'name'
     ];
+
+
+    public function jobs() : BelongsToMany
+    {
+        return $this->belongsToMany(Job::class);
+    }
 }
